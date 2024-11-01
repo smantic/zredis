847745f1c50b669780fbab137150d32293c0f364
smantic <tyler@smantic.dev>
2024-11-01 16:27:43 -0500

    parse request in loop

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const net = std.net;
+const eql = std.mem.eql;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
@@ -17,9 +18,29 @@ pub fn main() !void {
     while (true) {
         const connection = try listener.accept();
 
-        try connection.stream.writeAll("+PONG\r\n");
+        var buff: [1024]u8 = undefined;
+
+        while (true) {
+            const read_bytes = try connection.stream.read(&buff);
+            if (read_bytes == 0) break;
+            var iter = std.mem.tokenizeAny(u8, &buff, "\n\r");
+
+            while (iter.next()) |cmd| {
+                const resp = command(cmd);
+                try stdout.print("responded: {s}\n", .{resp});
+                try connection.stream.writeAll(resp);
+            }
+        }
 
         try stdout.print("accepted new connection", .{});
         connection.stream.close();
+    }
+}
+
+pub fn command(cmd: []const u8) []const u8 {
+    if (eql(u8, cmd, "PING")) {
+        return "+PONG\r\n";
+    } else {
+        return "";
     }
 }
